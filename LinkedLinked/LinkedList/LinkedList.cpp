88a2da2aecce54cacbb2bb96b275db575448edeb
SDeeze <davidseandunn@gmail.com>
2020-07-01 18:59:17 -0700

    Fixed runtime errors, made random sized list that generates random numbers for each node

--- LinkedLinked/LinkedList/LinkedList.cpp
+++ LinkedLinked/LinkedList/LinkedList.cpp
@@ -1,52 +1,82 @@
 ﻿// LinkedList.cpp : Defines the entry point for the application.
-//
 
 #include "LinkedList.h"
 
 using namespace std;
 
+class Node
+{
+public:
+	int data;
+	Node *next;
+};
+
+class List
+{
+public:
+	Node *head, *tail;
+
+public:
+	List()
+	{
+		head = NULL;
+		tail = NULL;
+	}
+
+	void CreateNode(int value)
+	{
+		Node *temp = new Node;
+		temp->data = value;
+		temp->next = NULL;
+		if (head == NULL)
+		{
+			head = temp;
+			tail = temp;
+			temp = NULL;
+		}
+		else
+		{
+			tail->next = temp;
+			tail = temp;
+		}
+	}
+};
+
+void print_list(List * list) {
+
+	Node *n = list->head;
+
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+	cout << endl;
+}
+
+int count_list(List * list) {
+	int size = 0;
+	Node *n = list->head;
+    while (n != NULL) {
+        size++;
+		n = n->next;
+    }
+	return size;
+}
+
 int main()
 {
 	cout << "Begin Program Execution" << endl;
-	createnode(15);
-	createnode(9);
+	List *mainList = new List();
+
+	int listLength = rand() % 10000;
+	for (int i = 0; i < listLength; i++)
+	{
+		mainList->CreateNode(rand() % 100000);
+	}
+
+	cout << "Final List" << endl;
+	print_list(mainList);
+	cout << "List Length: " << to_string(count_list(mainList)) << endl;
+	cout << "End Program Execution" << endl;
 	return 0;
 }
-
-struct Node { 
-public: 
-    int data; 
-    Node* next; 
-};
-
-class list
-  {
-    Private:
-    node *head, *tail;
-    public:
-    list()
-    {
-      head=NULL;
-      tail=NULL;
-    }
-	
-    void createnode(int value)
-	{
-		node *temp=new node;
-		temp->data=value;
-		temp->next=NULL;
-		if(head==NULL)
-		{
-			head=temp;
-			tail=temp;
-			temp=NULL;
-		}
-		else
-		{	
-			tail->next=temp;
-			tail=temp;
-		}
-	}
-  };
-
-
